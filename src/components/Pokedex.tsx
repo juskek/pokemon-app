ca69afa982258b87f09e3aa0b4ec9839c59a0312
Justin <57725347+juskek@users.noreply.github.com>
2023-06-06 11:30:52 +0100

    AAUser I can see a list of Pokemon names on the homescreen

--- src/components/Pokedex.tsx
+++ src/components/Pokedex.tsx
@@ -1,21 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { FlatList } from 'react-native';
+
+
+const pokemonList = require("../../assets/kanto.json");
+
+type ItemProps = { title: string };
+
+const Item = ({ title }: ItemProps) => (
+    <View style={styles.item}>
+        <Text style={styles.title}>{title}</Text>
+    </View>
+);
 
 export default function Pokedex() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.tsx to start working on your app!</Text>
+
+            <FlatList
+                data={pokemonList}
+                renderItem={({ item }) => <Item title={item.name} />}
+                keyExtractor={item => item.id}
+            />
+
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    item: {
+        backgroundColor: '#f9c2ff',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+    },
+    title: {
+        fontSize: 32,
+    },
+
 });
 
