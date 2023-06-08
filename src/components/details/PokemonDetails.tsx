54d58ff31c178c0fa19d188c011600a4b54bce1d
Justin <57725347+juskek@users.noreply.github.com>
2023-06-08 10:10:35 +0100

    AAU I can see the details of the Pokemon from the API
    sq

--- src/components/details/PokemonDetails.tsx
+++ src/components/details/PokemonDetails.tsx
@@ -1,15 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text} from 'react-native';
+import { ActivityIndicator, SafeAreaView, StyleSheet, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View } from 'react-native';
+
 
 
 export function PokemonDetails({ route }: { route: any, navigation: any }) {
     const { name, uri } = route.params;
+
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [data, setData] = useState<PokemonStats | null>(null);
+
+    const fetchDetails = async () => {
+        try {
+            const response = await fetch(uri)
+            const result = await response.json();
+
+            setData(result);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }
+
+    useEffect(() => {
+        fetchDetails().then(() => {
+            setIsLoading(false);
+        });
+    }, []);
+
 
     return (
         <SafeAreaView style={styles.container}>
             <Text>Pokemon Details</Text>
             <Text>{name}</Text>
             <Text>{uri}</Text>
+            {isLoading ? (
+                <ActivityIndicator />
+            ) : (
+                !data ? (
+                    <Text>No data</Text>
+                ) : (
+                    <View>
+                        <Text>Height: {data.height}</Text>
+                        <Text>Weight: {data.weight}</Text>
+                        <Text>Abilities: {data.abilities.map((ability) => ability.ability.name).join(', ')}</Text>
+                        <Text>Moves: {data.moves.map((move) => move.move.name).join(', ')}</Text>
+                    </View>
+                )
+            )}
             <StatusBar style="auto" />
         </SafeAreaView>
     );
