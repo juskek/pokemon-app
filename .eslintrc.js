af2651275c7cd6da5a693f2c5613174709f625f6
Justin <57725347+juskek@users.noreply.github.com>
2023-07-12 14:23:53 +0100

    Added linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,21 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
-    },
     "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:react/recommended",
+        "universe",
+        "universe/shared/typescript-analysis",
         "plugin:react-hooks/recommended",
-        "plugin:prettier/recommended",
-        "plugin:jsx-a11y/recommended"
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint",
-        "react",
-        "risxss/catch-potential-xss-react"
+    overrides: [
+        {
+            files: ["*.ts", "*.tsx", '*.d.ts'],
+            parserOptions: {
+                project: "./tsconfig.json",
+            }
+        }
     ],
-    "rules": {
-        "max-lines": ["error", { "max": 200, "skipBlankLines": true }],
-        "max-lines-per-function": ["error", { "max": 75, "skipBlankLines": true, "skipComments": true }],
-        "complexity": ["error", 6],
-        "react/react-in-jsx-scope": "off",
-        "no-restricted-syntax": ["error", {
-            "selector": "ExportDefaultDeclaration",
-            "message": "Prefer named exports"
-        }],
+    rules: {
+        "import/order": "off",
+        "react-hooks/exhaustive-deps": "error",
     }
+
+
 }
