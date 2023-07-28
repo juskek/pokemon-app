abfd87f181857c7877a88d7a9eafd334cfc4dfe7
Justin <57725347+juskek@users.noreply.github.com>
2023-07-28 10:52:47 +0100

    fix: added missing dep and env to babel config so that app builds

--- babel.config.js
+++ babel.config.js
@@ -2,14 +2,26 @@ module.exports = function (api) {
     api.cache(true);
 
     return {
-        presets: ["babel-preset-expo", "@babel/preset-env"],
-        plugins: [
-            [
-                "@babel/plugin-transform-private-property-in-object",
-                { loose: true },
-            ],
-            ["@babel/plugin-transform-private-methods", { loose: true }],
-            ["@babel/plugin-transform-class-properties", { loose: true }],
-        ],
+        env: {
+            production: { presets: ["babel-preset-expo"] },
+            development: { presets: ["babel-preset-expo"] },
+            test: {
+                presets: ["babel-preset-expo", "@babel/preset-env"],
+                plugins: [
+                    [
+                        "@babel/plugin-transform-private-property-in-object",
+                        { loose: true },
+                    ],
+                    [
+                        "@babel/plugin-transform-private-methods",
+                        { loose: true },
+                    ],
+                    [
+                        "@babel/plugin-transform-class-properties",
+                        { loose: true },
+                    ],
+                ],
+            },
+        },
     };
 };
